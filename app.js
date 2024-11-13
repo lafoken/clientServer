130bb5bb4d0eb09712f5c5a6c4134d32de9305cf
lafoken <oleksandrhybalo@gmail.com>
2024-11-13 22:31:20 +0100

    pr1

---
+++ app.js
@@ -0,0 +1,19 @@
+const http = require("http");
+
+const hostname = "0.0.0.0";
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/hello") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("Hello World!");
+  } else {
+    res.statusCode = 404;
+    res.end("Not Found");
+  }
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
